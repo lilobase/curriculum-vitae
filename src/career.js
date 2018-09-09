e9c94162e76658847ac4b0fecdbee886e467f055
lilobase <alemaire@quantis.fr>
2018-09-09 22:06:00 +0200

    init commit

---
+++ src/career.js
@@ -0,0 +1,16 @@
+// @flow
+
+type Company = {|
+    name: string,
+    summary: string,
+    description: string,
+    website: string
+|};
+
+type Role = {|
+
+|};
+
+type Career = {|
+
+|};
---
+++ src/common.js
@@ -0,0 +1,7 @@
+// @flow
+
+type highlight = string;
+export type highlights = highlight[];
+
+type tag = string | {| [string]: string | string[]|};
+export type tags = tag[];
